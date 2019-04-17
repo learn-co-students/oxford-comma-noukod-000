d1502865ac35a99a8a340f3460a3dfc24204bb70
Alexandro Disla <alexandrodisla@hotmail.com>
2019-04-17 20:20:28 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  newstring = ""
+  array_size = array.size
+  if array_size == 1
+    newstring << array[0]
+  elsif array_size == 2
+    newstring = array.join(" and ")
+  elsif array_size > 2
+    array[-1] = "and #{array[-1]}"
+  return  array.join(", ")
+  end
 end
