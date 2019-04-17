f6d50e9943e104db177cb93e3cc9eca1c0a996a0
Jerry Charles <jerrycharles586@gmail.com>
2019-04-17 12:10:39 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  n = array.length
+  str = array[0..n-2].join(", ")
+  if n == 2
+    str += " and #{array.last}"
+  elsif n > 2
+      str += ", and #{array.last}"
+  end
+  str
 end
+
+oxford_comma(["kiwi", "durian"])
