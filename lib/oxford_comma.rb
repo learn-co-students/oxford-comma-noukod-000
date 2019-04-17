535b3db8f4f6e071431328f98cb716fccc3e4898
Andy Feidje Joseph <ingandy75@gmail.com>
2019-04-17 04:03:55 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+    if array.size == 1
+    return  array.join
+    elsif array.size == 2
+    return  "#{array[0, array.length - 1].join(", ")} and #{array.last}"
+  elsif array.size == 3
+    return "#{array[0, array.length - 1].join(", ")}, and #{array.last}"
+  else
+    return "#{array[0, array.length - 1].join(", ")}, and #{array.last}"
+    end
 end
